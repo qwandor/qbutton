ee48696f4bb38878534b37cd8a0b2b7cbdbd0c0f
Andrew Walbran <qwandor@gmail.com>
2018-12-19 00:53:43 +0000

    Moving webserver code out to separate file.

--- src/main.cpp
+++ src/main.cpp
@@ -18,132 +18,16 @@ limitations under the License.
 #include "config.h"
 #include "logging.h"
 #include "streamutils.h"
+#include "webserver.h"
 #include "wifi.h"
 
 #include <ArduinoOTA.h>
 #include <DoubleResetDetect.h>
 #include <ESP8266mDNS.h>
-#include <ESP8266WebServer.h>
 #include <ESP8266WiFi.h>
 #include <FS.h>
 
-ESP8266WebServer server(80);
 DoubleResetDetect drd(DRD_TIMEOUT, DRD_ADDRESS);
-String admin_password;
-
-// Write the given command to /command.txt.
-bool update_command(const char *command) {
-  LOG("Updating command to \"");
-  LOG(command);
-  LOGLN("\"");
-
-  return write_line_to_file("/command.txt", command);
-}
-
-String load_command() {
-  return read_line_from_file("/command.txt");
-}
-
-///////////////////////////
-// HTTP request handlers //
-///////////////////////////
-
-void handle_root() {
-  if (admin_password.length() > 0 && !server.authenticate(ADMIN_USERNAME, admin_password.c_str())) {
-    server.requestAuthentication(DIGEST_AUTH, ADMIN_REALM);
-    return;
-  }
-
-  // If a new SSID and password have been sent, save them.
-  const String &new_admin_password = server.arg("admin_password");
-  const String &new_ssid = server.arg("ssid");
-  const String &new_password = server.arg("password");
-  const String &new_command = server.arg("command");
-  String error;
-  if (new_admin_password.length() > 0) {
-    if (write_line_to_file("/password.txt", new_admin_password.c_str())) {
-      admin_password = new_admin_password;
-    } else {
-      LOGLN("Failed to save new admin password.");
-      error = "Failed to save new admin password.";
-    }
-  }
-  if (new_ssid.length() > 0) {
-    File wifiFile = SPIFFS.open("/wifi.txt", "w");
-    if (!wifiFile) {
-      LOGLN("Failed to open /wifi.txt for writing.");
-      error = "Failed to open /wifi.txt for writing.";
-    }
-    // Don't use println, because it adds '\r' characters which we don't want.
-    wifiFile.print(new_ssid);
-    wifiFile.print('\n');
-    wifiFile.print(new_password);
-    wifiFile.print('\n');
-    wifiFile.close();
-    LOGLN("wrote new SSID and password");
-    LOGLN(new_ssid);
-  }
-  if (new_command.length() > 0) {
-    update_command(new_command.c_str());
-    auth_and_send_request(new_command);
-  }
-
-  // Read whatever is on disk.
-  String ssid, password;
-  File wifiFile = SPIFFS.open("/wifi.txt", "r");
-  if (!wifiFile) {
-    LOGLN("Failed to open /wifi.txt for reading.");
-  } else {
-    ssid = wifiFile.readStringUntil('\n');
-    password = wifiFile.readStringUntil('\n');
-    wifiFile.close();
-  }
-
-  String page = String("<html><head><title>qButton config</title></head><body><h1>qButton config</h1>") +
-    "<p style=\"color: red;\">" + error + "</p>" +
-    "<h2>Admin password</h2>" +
-    "<form method=\"post\" action=\"/\">" +
-    "<input type=\"text\" name=\"admin_password\" value=\"" + admin_password + "\"/><br/>" +
-    "<input type=\"submit\" value=\"Update admin password\"/>" +
-    "</form>" +
-    "<h2>Google account</h2>" +
-    "<a href=\"https://accounts.google.com/o/oauth2/v2/auth?client_id=" + client_id +
-    "&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fassistant-sdk-prototype&access_type=offline&response_type=code&redirect_uri=http://" +
-    WiFi.localIP().toString() + "/oauth&device_id=device_id&device_name=device_name\">Set account</a>" +
-    "<h2>WiFi config</h2>" +
-    "<form method=\"post\" action=\"/\">" +
-    "SSID: <input type=\"text\" name=\"ssid\" value=\"" + ssid + "\"/><br/>" +
-    "Password: <input type=\"text\" name=\"password\" value=\"" + password + "\"/><br/>" +
-    "<input type=\"submit\" value=\"Update WiFi config\"/>" +
-    "</form><form method=\"post\" action=\"/\">" +
-    "Command: <input type=\"text\" name=\"command\" value=\"" + load_command() + "\">" +
-    "<input type=\"submit\" value=\"Update command\"/>" +
-     "</form></body></html>";
-  server.send(200, "text/html", page);
-}
-
-void handle_oauth() {
-  const String &code = server.arg("code");
-  if (code.length() > 0) {
-    bool success = oauth_with_code(code);
-    if (success) {
-      server.send(200, "text/html", "<html><head><title>Auth</title></head><body><p>Success</p><a href=\"/\">Home</a></body></html>");
-    } else {
-      server.send(200, "text/html", "<html><head><title>Auth</title></head><body><p>Failure</p><a href=\"/\">Home</a></body></html>");
-    }
-  } else {
-    server.send(200, "text/html", String("<html><head><title>Error</title></head><body><h1>Authentication error</h1><p>") +
-      server.arg("error") + "</p></body></html>");
-  }
-}
-
-// Run web server to let the user authenticate their account.
-void start_webserver() {
-  server.on("/", handle_root);
-  server.on("/oauth", handle_oauth);
-  server.begin();
-  LOGLN("HTTP server started");
-}
 
 //////////////////
 // Entry points //
@@ -191,8 +75,6 @@ void setup() {
     LOGLN("Error starting mDNS");
   }
 
-  admin_password = read_line_from_file("/password.txt");
-
   start_webserver();
   #if OTA_UPDATE
   ArduinoOTA.setHostname(MDNS_HOSTNAME);
@@ -201,7 +83,7 @@ void setup() {
 }
 
 void loop() {
-  server.handleClient();
+  webserver_loop();
   #if OTA_UPDATE
   ArduinoOTA.handle();
   #endif
