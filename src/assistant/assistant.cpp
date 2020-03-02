71a15348ee34a91e18d3975258c2c8612225e671
Andrew Walbran <qwandor@gmail.com>
2020-03-02 19:49:39 +0000

    Update to new version of nanopb.

--- src/assistant/assistant.cpp
+++ src/assistant/assistant.cpp
@@ -148,15 +148,17 @@ bool encode_assist_request(pb_ostream_t *stream, const pb_field_t *field, void *
 // Encode the given command as the appropriate protobuf message and write it to the given Print stream.
 bool encode_request(const char *command, pb_ostream_t *pb_out) {
   google_assistant_embedded_v1alpha2_AssistRequest assist_request = google_assistant_embedded_v1alpha2_AssistRequest_init_default;
-  assist_request.config.audio_out_config.encoding = google_assistant_embedded_v1alpha2_AudioOutConfig_Encoding_MP3;
-  assist_request.config.audio_out_config.sample_rate_hertz = 16000;
-  assist_request.config.audio_in_config.encoding = google_assistant_embedded_v1alpha2_AudioInConfig_Encoding_LINEAR16;
-  assist_request.config.audio_in_config.sample_rate_hertz = 16000;
-  assist_request.config.screen_out_config.screen_mode = google_assistant_embedded_v1alpha2_ScreenOutConfig_ScreenMode_PLAYING;
-  safe_copy("en-US", assist_request.config.dialog_state_in.language_code);
-  safe_copy(device_id, assist_request.config.device_config.device_id);
-  safe_copy(device_model_id, assist_request.config.device_config.device_model_id);
-  safe_copy(command, assist_request.config.text_query);
+  assist_request.which_type = google_assistant_embedded_v1alpha2_AssistRequest_config_tag;
+  assist_request.type.config.audio_out_config.encoding = google_assistant_embedded_v1alpha2_AudioOutConfig_Encoding_MP3;
+  assist_request.type.config.audio_out_config.sample_rate_hertz = 16000;
+  assist_request.type.config.which_type = google_assistant_embedded_v1alpha2_AssistConfig_audio_in_config_tag;
+  assist_request.type.config.type.audio_in_config.encoding = google_assistant_embedded_v1alpha2_AudioInConfig_Encoding_LINEAR16;
+  assist_request.type.config.type.audio_in_config.sample_rate_hertz = 16000;
+  assist_request.type.config.screen_out_config.screen_mode = google_assistant_embedded_v1alpha2_ScreenOutConfig_ScreenMode_PLAYING;
+  safe_copy("en-US", assist_request.type.config.dialog_state_in.language_code);
+  safe_copy(device_id, assist_request.type.config.device_config.device_id);
+  safe_copy(device_model_id, assist_request.type.config.device_config.device_model_id);
+  safe_copy(command, assist_request.type.config.type.text_query);
 
   google_rpc_StreamBody stream_body = google_rpc_StreamBody_init_default;
   stream_body.message.funcs.encode = encode_assist_request;
